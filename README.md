0b32cbc4ae70c589740b2b3cb6cafec4b6efebac
Nattugle <business.sandernkl@gmail.com>
2023-04-07 19:20:16 +0200

    first commit vs

---
+++ README.md
@@ -0,0 +1,7 @@
+# pretty-random
+
+Examples:
+- random.choice(['Banana', 'Strawberry'])
+- random.randint(1, 10)
+
+Install with `npm install pretty-random`
---
+++ src/index.js
@@ -0,0 +1,44 @@
+function choice(array) {
+    /* 
+    Allows you to pick something randomly from an array,
+    like in python!
+    */
+
+    if (!Array.isArray(array)) {
+        throw new Error(`Choice parameter must be an array, but the given parameter was not. (${array})`);
+    };
+
+    const random = Math.floor(Math.random() * array.length);
+    return array[random]
+}
+
+
+function randInt(l, h) {
+    /* 
+    Javascript random, python style.
+    Picks a random number from lowest (l) to highest (h).
+    */
+
+    if (isNaN(l) || isNaN(h)) {
+        throw new Error(`Received NaN in parameters. (${l}, ${h}, )`);
+    };
+
+    const number = Math.floor(Math.random() * (h - l + 1) + l)
+    return number
+}
+
+function uniform(l, h) {
+    /* 
+    Picks a random number with decimals between lowest (l) to highest (h)
+    */
+
+    if (isNaN(l) || isNaN(h)) {
+        throw new Error(`Received NaN in parameters. (${l}, ${h}, )`);
+    };
+
+    const number = Math.random() * (h - l) + l
+    return number
+}
+
+
+module.exports = { choice, randInt };
