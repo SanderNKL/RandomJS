fd24d2eb9568986d9a069a1273356280c2f08a44
Nattugle <business.sandernkl@gmail.com>
2023-04-08 20:03:15 +0200

    added shuffle function

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # pretty-random
 
-Examples:
+## Examples:
 - random.choice(['Banana', 'Strawberry']) Returns an item from the array
 - random.randint(1, 10) Returns an integer number
 - random.uniform(1, 10) Returns a decimal number
+- random.shuffle(['item1', 'item2']) Returns a shuffled list. For example: ['item2', 'item1']
 
 Install with `npm install pretty-random`
--- src/index.js
+++ src/index.js
@@ -1,16 +1,48 @@
+/*
+This application is inspired by the Python "RANDOM" Library.
+As this does not exist by default in JAVASCRIPT, this application will
+allow you to use all of those functions without having to type "hard to remember"
+code which does the exact same thing. Keep it simple, right?
+*/
+
 function choice(array) {
     /* 
-    Allows you to pick something randomly from an array,
-    like in python!
+    Picks a random item from the array.
     */
 
     if (!Array.isArray(array)) {
         throw new Error(`Choice parameter must be an array, but the given parameter was not. (${array})`);
     };
 
+    if (array.length < 1) {
+        throw new Error('Cannot choose from an empty array');
+    }
+
     const random = Math.floor(Math.random() * array.length);
-    return array[random]
+    return array[random];
 }
+
+function shuffle(array) {
+    /*
+    Shuffles your array at random, and returns that array
+    */
+
+    if (!Array.isArray(array)) {
+        throw new Error(`Choice parameter must be an array, but the given parameter was not. (${array})`);
+    };
+
+    var al = array.length;
+
+    // While the array is not empty
+    while (al) {
+        i = Math.floor(Math.random() * al--);
+        t = array[al];
+        array[al] = array[i];
+        array[i] = t;
+    }
+
+    return array;
+};
 
 
 function randInt(l, h) {
@@ -19,16 +51,16 @@ function randInt(l, h) {
     Picks a random number from lowest (l) to highest (h).
     */
 
-    h = Math.floor(h)
-    l = Math.floor(l)
+    h = Math.floor(h);
+    l = Math.floor(l);
 
     if (isNaN(l) || isNaN(h)) {
         throw new Error(`Received NaN in parameters. (${l}, ${h}, )`);
     };
 
-    const number = Math.floor(Math.random() * (h - l + 1) + l)
-    return number
-}
+    const number = Math.floor(Math.random() * (h - l + 1) + l);
+    return number;
+};
 
 function uniform(l, h) {
     /* 
@@ -39,8 +71,8 @@ function uniform(l, h) {
         throw new Error(`Received NaN in parameters. (${l}, ${h}, )`);
     };
 
-    const number = Math.random() * (h - l) + l
-    return number
-}
+    const number = Math.random() * (h - l) + l;
+    return number;
+};
 
-module.exports = { choice, randInt, uniform};
+module.exports = { choice, randInt, uniform, shuffle};
